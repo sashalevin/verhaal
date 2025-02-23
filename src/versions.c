88908f7d5a6b40fae5e5f107bbde6166e1a3a32b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2025-02-23 12:54:44 +0100

    versions: properly add the "last" -rc version
    The last -rc version was not being properly added to the db, do that
    now.  Yes, the versions logic needs to be redone (it's ranges, not
    releases that we care about), but that's not for this commit just yet.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- src/versions.c
+++ src/versions.c
@@ -124,6 +124,115 @@ static void loop_through_x(int major)
 
 }
 
+static int loop_through_rc(void)
+{
+	git_describe_result *res;
+	git_object *object;
+	git_reference *ref;
+	git_describe_format_options options;
+	git_describe_options desc_options;
+	const git_oid *oid;
+	git_buf buf = { 0 };
+	char *head_tag;
+	int ret;
+	char range1[256];
+	char range2[256];
+
+	// First, find the current release, which is the "newest" tag (i.e. description) of the head
+	// that is in the master branch. "git describe --abbrev=0 master" does what we need.
+
+#if 0
+	// Cheat way, just call popen()!
+	FILE *fp;
+	char output[1024];
+	fp = popen("cd /home/gregkh/linux/stable/linux-stable/ && git describe --abbrev=0", "r");
+	fgets(output, 1024, fp);
+	pclose(fp);
+	fprintf(stdout, "result = %s", output);
+#endif	// ok, let do it right...
+
+	// Get the branch description of 'master'
+	ret = git_branch_lookup(&ref, git_repo, "master", GIT_BRANCH_LOCAL);
+	if (ret)
+		fprintf(stderr, "git_branch_lookup() failed: %d\n", ret);
+
+	oid = git_reference_target(ref);
+	if (!oid)
+		fprintf(stderr, "git_reference_target() failed\n");
+
+	ret = git_object_lookup(&object, git_repo, oid, GIT_OBJECT_COMMIT);
+	if (ret)
+		fprintf(stderr, "git_object_lookup() failed: %d\n", ret);
+
+
+	git_describe_options_init(&desc_options, GIT_DESCRIBE_OPTIONS_VERSION);
+	desc_options.only_follow_first_parent = 1;
+
+	ret = git_describe_commit(&res, object, &desc_options);
+	if (ret)
+		fprintf(stderr, "git_describe_commit() failed: %d\n", ret);
+
+	git_describe_format_options_init(&options, GIT_DESCRIBE_FORMAT_OPTIONS_VERSION);
+	options.abbreviated_size = 0;
+
+	ret = git_describe_format(&buf, res, &options);
+	if (ret)
+		fprintf(stderr, "git_describe_format() failed: %d\n", ret);
+
+	terminal_fprintf(stdout, "  git head tag = " TERMINAL_FG_CYAN "%s" TERMINAL_FG_DEFAULT "\n",
+			 buf.ptr);
+
+	head_tag = strdup(buf.ptr);
+
+	git_describe_result_free(res);
+	git_object_free(object);
+	git_reference_free(ref);
+	git_buf_dispose(&buf);
+
+	// If there is no "-rc" in the head, then nothing to do!
+	char *rc = strstr(head_tag, "-rc");
+	if (rc == NULL) {
+		fprintf(stdout, "At a main release, no -rc release to generate.\n");
+		goto exit;
+	}
+
+	// Let's start with the previous release and go to the current -rc1
+	// Carve off the -rc from the string
+	rc[0] = 0x00;
+
+	// Find the first '.'
+	char *dot = strstr(head_tag, ".");
+	if (dot == NULL) {
+		fprintf(stderr, "Error: can not parse version string '%s', exiting -rc attempt\n",
+			head_tag);
+		goto exit;
+	}
+
+	// Find the major and minor number of the release
+	int major = atoi(&head_tag[1]);
+	int minor = atoi(&dot[1]);
+
+	snprintf(range1, sizeof(range1), "%d.%d", major, minor - 1);
+	snprintf(range2, sizeof(range2), "%d.%d-rc1", major, minor);
+	add_version_major(range2);
+
+	// Let's walk through as many -rc releases as we can think of
+	for (int i = 1; i < 12; ++i) {
+		snprintf(range1, sizeof(range1), "v%s-rc%d", &head_tag[1], i);
+		snprintf(range2, sizeof(range2), "v%s-rc%d", &head_tag[1], i + 1);
+		if (!is_valid_release(range1) || !is_valid_release(range2))
+			continue;
+
+		snprintf(range1, sizeof(range1), "%s-rc%d", &head_tag[1], i);
+		snprintf(range2, sizeof(range2), "%s-rc%d", &head_tag[1], i + 1);
+		add_version_major(range2);
+	}
+
+exit:
+	free(head_tag);
+	return 0;
+}
+
 void versions_create(void)
 {
 	struct vh_timestamp *foo;
@@ -135,6 +244,7 @@ void versions_create(void)
 	loop_through_x(4);
 	loop_through_x(5);
 	loop_through_x(6);
+	loop_through_rc();
 	seconds = time_stop(foo);
 	terminal_fprintf(stdout, "    Versions create took "
 			 TERMINAL_FG_CYAN "%.5f" TERMINAL_FG_DEFAULT
